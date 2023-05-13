b8a5d453641f70b552b0b7686854fcf4547153ec
Sidemar Rosa <102702961+SidemarRosa@users.noreply.github.com>
2023-05-13 01:41:59 -0300

    Projeto final
    Adições das ações javascript e tapa final no visual

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
+let contador = 0;
+let elementoContador = document.getElementById("contador");
 
-    
+document.getElementById("adicionar").addEventListener("click", function() {
+  contador++;
+  elementoContador.textContent = contador;
+
+});
+
+document.getElementById("subtrair").addEventListener("click", function() {
+  contador--;
+  elementoContador.textContent = contador;
+
+});
+
+document.getElementById("resetar").addEventListener("click", function() {
+  contador = 0;
+  elementoContador.textContent = contador;
+
+});
